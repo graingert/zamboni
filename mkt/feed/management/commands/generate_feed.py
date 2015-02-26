600d110b4135d393f5b7e6d599d84a067bc54d0f
Allen Short <ashort@mozilla.com>
2015-02-26 15:21:36 -0800

    generate fake data for feed (bug 1131903)

---
+++ mkt/feed/management/commands/generate_feed.py
@@ -0,0 +1,19 @@
+from optparse import make_option
+
+from django.core.management.base import BaseCommand, CommandError
+
+from mkt.feed.fakedata import generate_feed_data
+
+
+class Command(BaseCommand):
+    """
+    Usage:
+
+        python manage.py generate_feed
+
+    """
+
+    help = 'Generate example feed data for development/testing'
+
+    def handle(self, *args, **kwargs):
+        generate_feed_data()
